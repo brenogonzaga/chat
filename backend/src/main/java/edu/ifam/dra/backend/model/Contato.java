aa900ba1da0f214bbabb85cc007bcbda0b91c284
Breno Gonzaga <contato@brenogonzaga.com>
2023-11-20 14:49:53 -0400

    update pom.xml and fix issues related

--- backend/src/main/java/edu/ifam/dra/backend/model/Contato.java
+++ backend/src/main/java/edu/ifam/dra/backend/model/Contato.java
@@ -1,29 +1,28 @@
 package edu.ifam.dra.backend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Contato {
-	
+
 	/*
-	{
-        "nome": "Isadora",
-        "email": "isadora@ifam.edu.br",
-        "telefone" : "(92) 99999-8888",
-        "endereco" : "Av. 7 de Setembro, 1975",
-        "bairro" : "Centro",
-        "cidade" : "Manaus",
-        "estado" : "AM"
-    }
+	 * {
+	 * "nome": "Isadora",
+	 * "email": "isadora@ifam.edu.br",
+	 * "telefone" : "(92) 99999-8888",
+	 * "endereco" : "Av. 7 de Setembro, 1975",
+	 * "bairro" : "Centro",
+	 * "cidade" : "Manaus",
+	 * "estado" : "AM"
+	 * }
 	 */
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	private String nome;
 	private String email;
 	private String telefone;
@@ -32,8 +31,6 @@ public class Contato {
 	private String cidade;
 	private String estado;
 
-	public Contato() {}
-	
 	public Contato(String nome, String email, String telefone, String endereco, String bairro, String cidade,
 			String estado) {
 		super();
@@ -45,58 +42,5 @@ public class Contato {
 		this.cidade = cidade;
 		this.estado = estado;
 	}
-	
-	
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	public String getBairro() {
-		return bairro;
-	}
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-	public String getCidade() {
-		return cidade;
-	}
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-	public String getEstado() {
-		return estado;
-	}
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-	
-	
 }
--- frontend/src/main/java/edu/ifam/dra/front/controller/ContatoFrontController.java
+++ frontend/src/main/java/edu/ifam/dra/front/controller/ContatoFrontController.java
@@ -5,12 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import edu.ifam.dra.front.model.Contato;
 import edu.ifam.dra.front.service.ContatoFrontService;
@@ -18,52 +13,48 @@ import edu.ifam.dra.front.service.ContatoFrontService;
 @RequestMapping("/contato")
 @Controller
 public class ContatoFrontController {
-	
+
 	@Autowired
 	ContatoFrontService contatoFrontService;
 
 	@GetMapping
 	public String mostraContatos(Model model) {
-		
 		model.addAttribute("contatos", contatoFrontService.getContatos());
 		return "listaContatos";
-		
+
 	}
-	
+
 	@GetMapping("/novo")
 	public String novoContato(Contato contato) {
 		return "novoContato";
 	}
-	
+
 	@PutMapping("/alterar/{id}")
-	public String alterarContato(@PathVariable Long id,
-			@Validated Contato contato, BindingResult result, Model model) {
-		
+	public String alterarContato(@PathVariable Long id, @Validated Contato contato, BindingResult result, Model model) {
 		model.addAttribute("contato", contatoFrontService.getContato(id));
 		return "alterarContato";
 	}
 
 	@PostMapping
-    public String inserirContato(@Validated Contato contato, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "novoContato";
-        }
-        
-        contatoFrontService.postContato(contato);
-        return "redirect:/contato";
-    }
-    
-    @PutMapping("/{id}")
-    public String alterarContato(@PathVariable long id,
-    		@Validated Contato contato, BindingResult result, Model model) {
-    	contatoFrontService.putContato(id, contato);
-    	return "redirect:/contato";
-    }
-    
-    @DeleteMapping("/{id}")
-    public String excluirContato(@PathVariable long id) {
-    	contatoFrontService.deleteContato(id);
-    	return "redirect:/contato";
-    }
-    
+	public String inserirContato(@Validated Contato contato, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "novoContato";
+		}
+		contatoFrontService.postContato(contato);
+		return "redirect:/contato";
+	}
+
+	@PutMapping("/{id}")
+	public String alterarContato(@PathVariable long id,
+			@Validated Contato contato, BindingResult result, Model model) {
+		contatoFrontService.putContato(id, contato);
+		return "redirect:/contato";
+	}
+
+	@DeleteMapping("/{id}")
+	public String excluirContato(@PathVariable long id) {
+		contatoFrontService.deleteContato(id);
+		return "redirect:/contato";
+	}
+
 }
